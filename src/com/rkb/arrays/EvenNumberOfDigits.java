55dfaba8df825ae994532e8c918cd186adc81a41
Ram <ramkb466@gmail.com>
2020-02-18 12:48:05 -0500

    solved find even numbers of digits

---
+++ src/com/rkb/arrays/EvenNumberOfDigits.java
@@ -0,0 +1,40 @@
+package com.rkb.arrays;
+
+public class EvenNumberOfDigits {
+	
+	/**
+	 * Given an array nums of integers, return how many of them contain an even number of digits.
+ 
+
+Example 1:
+
+Input: nums = [12,345,2,6,7896]
+Output: 2
+Explanation: 
+12 contains 2 digits (even number of digits). 
+345 contains 3 digits (odd number of digits). 
+2 contains 1 digit (odd number of digits). 
+6 contains 1 digit (odd number of digits). 
+7896 contains 4 digits (even number of digits). 
+Therefore only 12 and 7896 contain an even number of digits.
+Example 2:
+
+Input: nums = [555,901,482,1771]
+Output: 1 
+Explanation: 
+Only 1771 contains an even number of digits.
+	 * @param nums
+	 * @return
+	 */
+
+	public int findNumbers(int[] nums) {
+        int count = 0;
+        for(int i = 0; i<nums.length;i++) {
+            String s = Integer.toString(nums[i]);
+        	if(s.length() % 2 == 0) {
+        		count++;
+        	}
+        }
+        return count;
+	}
+}
