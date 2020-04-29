23d2f05d0873bf49c4637507ab830cc86d35fbba
Ram <ramkb466@gmail.com>
2020-04-29 07:55:32 -0400

    2nd apprach

--- src/com/rkb/strings/ReverseOnlyLetters.java
+++ src/com/rkb/strings/ReverseOnlyLetters.java
@@ -22,5 +22,22 @@ public class ReverseOnlyLetters {
 
 	        return ans.toString();
 	    }
+	 
+	 // approach 2
+	 public String reverseOnlyLetters2(String S) {
+	        StringBuilder ans = new StringBuilder();
+	        int j = S.length() - 1;
+	        for (int i = 0; i < S.length(); ++i) {
+	            if (Character.isLetter(S.charAt(i))) {
+	                while (!Character.isLetter(S.charAt(j)))
+	                    j--;
+	                ans.append(S.charAt(j--));
+	            } else {
+	                ans.append(S.charAt(i));
+	            }
+	        }
+
+	        return ans.toString();
+	    }
 
 }
