d69419ef0b8cc8880080bef64293634f7ff9715d
Ram <ramkb466@gmail.com>
2020-05-13 08:08:46 -0400

    2nd approach

--- src/binarySearch/SearchInsert.java
+++ src/binarySearch/SearchInsert.java
@@ -39,4 +39,25 @@ public class SearchInsert {
         }
         return 0;       
     }
+	
+	// 2nd approach
+	public int searchInsert2(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int mid = 0;
+        while (start <= end) {
+            mid = (start + end) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+       if (target < nums[mid])
+           return mid;
+        else 
+            return mid + 1;
+    }
 }
