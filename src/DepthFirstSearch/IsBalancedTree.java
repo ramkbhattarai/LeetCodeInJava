a9582be90a2e784679726fbd2e360ef0ab0cbafc
Ram <ramkb466@gmail.com>
2020-05-30 22:14:20 -0400

    1st appraoch

---
+++ src/DepthFirstSearch/IsBalancedTree.java
@@ -0,0 +1,21 @@
+package DepthFirstSearch;
+
+public class IsBalancedTree {
+	// 1st approach
+	public boolean isBalanced(TreeNode root) {
+		if (root == null){
+		return true;
+		}
+		if (Math.abs(depth(root.left) - depth(root.right)) > 1){
+		return false;
+		}
+		return isBalanced(root.left) && isBalanced(root.right);
+		}
+		public int depth(TreeNode root){
+		if (root == null){
+		return 0;
+		}
+		return Math.max(depth(root.left),depth(root.right)) + 1;
+		}
+
+}
