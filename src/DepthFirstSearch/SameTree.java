50935cba6a16675f0eee2d1e0e33a9eb98abbbd8
Ram <ramkb466@gmail.com>
2020-06-11 10:26:21 -0400

    1 approach

---
+++ src/DepthFirstSearch/SameTree.java
@@ -0,0 +1,51 @@
+package DepthFirstSearch;
+
+/**
+ * Given two binary trees, write a function to check if they are the same or not.
+
+Two binary trees are considered the same if they are structurally identical and the nodes have the same value.
+
+Example 1:
+
+Input:     1         1
+          / \       / \
+         2   3     2   3
+
+        [1,2,3],   [1,2,3]
+
+Output: true
+Example 2:
+
+Input:     1         1
+          /           \
+         2             2
+
+        [1,2],     [1,null,2]
+
+Output: false
+Example 3:
+
+Input:     1         1
+          / \       / \
+         2   1     1   2
+
+        [1,2,1],   [1,1,2]
+
+Output: false
+ * @author ramkrishnabhattarai
+ *
+ */
+
+public class SameTree {
+
+	// 1 approach
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+	    // p and q are both null
+	    if (p == null && q == null) return true;
+	    // one of p and q is null
+	    if (q == null || p == null) return false;
+	    if (p.val != q.val) return false;
+	    return isSameTree(p.right, q.right) &&
+	            isSameTree(p.left, q.left);
+	  }
+}
