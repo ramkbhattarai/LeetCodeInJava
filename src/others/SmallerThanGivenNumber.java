32cc45c4b5a27d04263a88f2d1ab583ad6aa8944
Ram <ramkb466@gmail.com>
2020-06-16 22:08:41 -0400

    2nd approach

--- src/others/SmallerThanGivenNumber.java
+++ src/others/SmallerThanGivenNumber.java
@@ -60,5 +60,21 @@ public class SmallerThanGivenNumber {
         return nums;
 		
 	}
+	
+	 public int[] smallerNumbersThanCurrent(int[] nums) {
+	        int len = nums.length;
+	        int[] result = new int[len];
+	        List<Integer> temp = new ArrayList<>();
+
+	        for(int n : nums) temp.add(n);
+
+	        Collections.sort(temp);
+
+	        for (int i = 0 ; i < len ; i++) {
+	            result[i] = temp.indexOf(nums[i]);
+	        }
+
+	        return result;
+	    }
 
 }
