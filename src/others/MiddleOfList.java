3ffc8a15b7f2cae7dea0d7264672ed9a8d451a6f
Ram <ramkb466@gmail.com>
2020-06-23 10:42:22 -0400

    3rd approach

--- src/others/MiddleOfList.java
+++ src/others/MiddleOfList.java
@@ -43,5 +43,20 @@ public class MiddleOfList {
 	        }
 	        return slow;
 	    }
+	 
+	 
+	 public ListNode middleNode(ListNode head) {
+	        
+	        ListNode fast = head;
+	        ListNode slow = head;
+	        
+	        while (fast != null && fast.next != null) {
+	            slow = slow.next;
+	            fast = fast.next.next;
+	        }
+	        
+	        return slow;
+	        
+	    }
 
 }
