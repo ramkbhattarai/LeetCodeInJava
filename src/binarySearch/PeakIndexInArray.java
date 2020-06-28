db4b494b262027d1ee6f9a7efb288d87fcac2627
Ram <ramkb466@gmail.com>
2020-06-28 13:06:49 -0400

    2nd approach

--- src/binarySearch/PeakIndexInArray.java
+++ src/binarySearch/PeakIndexInArray.java
@@ -31,4 +31,16 @@ public class PeakIndexInArray {
 	        return i;
 	    }
 	 
+	 
+	 public int peakIndexInMountainArray2(int[] A) {
+	        int lo = 0, hi = A.length - 1;
+	        while (lo < hi) {
+	            int mi = lo + (hi - lo) / 2;
+	            if (A[mi] < A[mi + 1])
+	                lo = mi + 1;
+	            else
+	                hi = mi;
+	        }
+	        return lo;
+	    }
 }
