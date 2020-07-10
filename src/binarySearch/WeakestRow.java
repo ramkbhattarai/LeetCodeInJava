0fb9a5941f07a8dfb25571b3c26b74004a5a066d
Ram <ramkb466@gmail.com>
2020-07-09 21:41:51 -0400

    2nd approach

--- src/binarySearch/WeakestRow.java
+++ src/binarySearch/WeakestRow.java
@@ -75,5 +75,36 @@ public class WeakestRow {
 	        }
 	       return ans;
 	    }
+	 
+	 public int[] kWeakestRows2(int[][] mat, int k) {
+	        int[] result = new int[k];
+
+	        //gather soldiers count into single dimensional array(per row)
+	        int[] counts = new int[mat.length];
+	        for (int i = 0; i < mat.length; i++) {
+	            for (int j = 0; j < mat[i].length; j++) {
+	                if (mat[i][j] == 1) {
+	                    counts[i]++;
+	                }
+	            }
+	        }
+
+	        int indexer = 0;
+	        while (k > 0) {
+	            //make linear search k times and take minimum index to fill in result array
+	            int minIndex = 0;
+	            for (int i = 0; i < counts.length; i++) {
+	                if (counts[i] < counts[minIndex]) {
+	                    minIndex = i;
+	                }
+	            }
+
+	            result[indexer++] = minIndex;
+	            counts[minIndex] = Integer.MAX_VALUE;
+	            k--;
+	        }
+
+	        return result;
+	    }
 
 }
