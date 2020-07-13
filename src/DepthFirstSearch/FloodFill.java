11f957a0400c313e62f0c34bcda23abd0370437e
Ram <ramkb466@gmail.com>
2020-07-12 21:22:47 -0400

    2nd approach

--- src/DepthFirstSearch/FloodFill.java
+++ src/DepthFirstSearch/FloodFill.java
@@ -39,5 +39,26 @@ public class FloodFill {
             if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
         }
     }
+    
+    
+    public int[][] floodFill2(int[][] image, int sr, int sc, int newColor) {
+        int oldColor = image[sr][sc];
+        if(oldColor!= newColor){
+        image[sr][sc] = newColor;
+            if(sr - 1 >= 0 && image[sr - 1][sc] == oldColor){
+                floodFill(image, sr-1, sc, newColor);
+            }
+            if(sc - 1 >= 0 && image[sr][sc- 1] == oldColor){
+                floodFill(image, sr, sc-1, newColor);
+            }
+            if(sr + 1 < image.length && image[sr + 1][sc] == oldColor){
+                floodFill(image, sr+1, sc, newColor);
+            }
+            if(sc + 1 < image[0].length && image[sr][sc+1] == oldColor){
+                floodFill(image, sr, sc+1, newColor);
+            }
+        }
+        return image;
+    }
 
 }
