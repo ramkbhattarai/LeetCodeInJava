79df92140151b5cd7d2d1a21521db41d124ed399
Ram <ramkb466@gmail.com>
2020-08-10 23:08:04 -0400

    2nd approach

--- src/others/WordBreak.java
+++ src/others/WordBreak.java
@@ -18,5 +18,27 @@ public class WordBreak {
         
         return dp[s.length()];
     }
+	
+	
+	
+	public boolean wordBreak(String s, List<String> wordDict) {        
+        return bckTrck(s, new HashSet<>(wordDict), 0, new Boolean[s.length()]);
+    }
+    
+    public boolean bckTrck(String s, Set<String> dict, int idx, Boolean[] memo) {
+        if (idx == s.length()) return true;
+        
+        if (memo[idx] != null) return memo[idx];
+        
+        for (int end = idx + 1; end <= s.length(); end++) {
+            if (dict.contains(s.substring(idx, end)) && bckTrck(s, dict, end, memo)){
+                memo[idx] = true;
+                return true;
+            } 
+        }
+        
+        memo[idx] = false;
+        return false;
+    }
 
 }
