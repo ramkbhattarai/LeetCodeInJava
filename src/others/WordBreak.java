a59cfaafa326491e6f06857826ae7fef63d76222
Ram <ramkb466@gmail.com>
2020-08-10 23:09:05 -0400

    3rd approach

--- src/others/WordBreak.java
+++ src/others/WordBreak.java
@@ -40,5 +40,32 @@ public class WordBreak {
         memo[idx] = false;
         return false;
     }
+    
+    
+    public boolean wordBreak(String s, List<String> wordDict) {        
+        Set<String> dict = new HashSet<>(wordDict);
+        
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[s.length()];
+        q.offer(0);
+        
+        while(!q.isEmpty()) {
+            int start = q.poll();
+            
+            if (!visited[start]) {
+                for (int end = start + 1; end <= s.length(); end++) {
+                    if (dict.contains(s.substring(start, end))) {  
+                        if (end == s.length()) return true;
+                        
+                        q.offer(end);
+                    }
+                }
+                
+                visited[start] = true;
+            }
+        }
+        
+        return false;
+    }
 
 }
