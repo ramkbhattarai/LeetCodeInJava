35148a4018e8dcbab02111775ccb3c3c280c2208
Ram <ramkb466@gmail.com>
2020-08-16 20:04:15 -0400

    dp approach

--- src/others/ClimbingStairs.java
+++ src/others/ClimbingStairs.java
@@ -19,5 +19,18 @@ public class ClimbingStairs {
         memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
         return memo[i];
     }
+    
+    public int climbStairs2(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
+    }
 
 }
