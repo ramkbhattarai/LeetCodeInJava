1f1a2a2c6d2e12c17a2729b4e5b6b4d00a50ebb5
Ram <ramkb466@gmail.com>
2020-08-16 20:05:02 -0400

    fibonachi approach

--- src/others/ClimbingStairs.java
+++ src/others/ClimbingStairs.java
@@ -32,5 +32,19 @@ public class ClimbingStairs {
         }
         return dp[n];
     }
+    
+    public int climbStairs3(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        int first = 1;
+        int second = 2;
+        for (int i = 3; i <= n; i++) {
+            int third = first + second;
+            first = second;
+            second = third;
+        }
+        return second;
+    }
 
 }
