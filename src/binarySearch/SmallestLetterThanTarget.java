876fe80eeab423ec893647274ae48732c25be93d
Ram <ramkb466@gmail.com>
2020-09-04 21:51:36 -0400

    another way

--- src/binarySearch/SmallestLetterThanTarget.java
+++ src/binarySearch/SmallestLetterThanTarget.java
@@ -71,5 +71,30 @@ public class SmallestLetterThanTarget {
 	        }
 	        return letters[lo % letters.length];
 	    }
+	 
+	 public char nextGreatestLetter4(char[] letters, char target) {
+	        char res=letters[0];
+	        int start=0;
+	        int end=letters.length-1;
+	        while(start<=end)
+	        {
+	            int mid=start+(end-start)/2;
+	            if(letters[mid]==target)
+	            {
+	                start=mid+1;
+	            }
+	            else if(target>letters[mid])
+	            {
+	                start=mid+1;
+	            }
+	            else if(letters[mid]>target)
+	            {
+	                res=letters[mid];
+	                end=mid-1;
+	            }
+	        }
+	        return res;
+	        
+	    }
 
 }
